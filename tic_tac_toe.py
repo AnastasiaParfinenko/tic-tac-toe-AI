a17f3f736e49e6825cfdae686a20c11b07859125
Anastasia Parfinenko <anastasia.s.parfinenko@gmail.com>
2024-07-05 11:57:19 +0200

    Implemented choice players, but start of a new game doesn't work normal
    According to the third stage of Hyperskill

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,17 +1,3 @@
-def create_table(state):
-    global count_moves, size
-
-    state_table = [[' '] * size for _ in range(size)]
-
-    for i in range(size):
-        for j in range(size):
-            if state[i * size + j] != '_':
-                state_table[i][j] = state[i * size + j]
-                count_moves += 1
-
-    return state_table
-
-
 # def state_analyze(state):
 #     if abs(state.count('X') - state.count('O')) > 1 or (win(state, 'O') and win(state, 'X')):
 #         print('Impossible')
@@ -25,6 +11,51 @@ def create_table(state):
 #         print('Game not finished')
 #
 
+class User:
+    name = 'User'
+
+    def move(self):
+        while True:
+            coordinates = input('Enter the coordinates: ')
+            try:
+                x, y = map(int, coordinates.split())
+            except Exception:
+                print('You should enter numbers!')
+            else:
+                if not (x in range(1, size + 1) and y in range(1, size + 1)):
+                    print(f'Coordinates should be from 1 to {size}!')
+                elif state_table[x - 1][y - 1] == ' ':
+                    return x, y
+                else:
+                    print('This cell is occupied! Choose another one!')
+
+
+class Easy:
+    name = 'Easy'
+
+    def move(self):
+        import random
+        import time
+
+        print('Making move level "easy"')
+        time.sleep(0.9)
+
+        return random.choice(free_cells)
+
+
+def create_table(state):
+    global count_moves
+
+    state_table = [[' '] * size for _ in range(size)]
+
+    for i in range(size):
+        for j in range(size):
+            if state[i * size + j] != '_':
+                state_table[i][j] = state[i * size + j]
+                count_moves += 1
+
+    return state_table
+
 
 def illustrate(state_table):
     print('---' * size)
@@ -36,36 +67,35 @@ def illustrate(state_table):
     print('---' * size)
 
 
-def move(coordinates, symbol):
+# def easy_move(symbol):
+#     import random
+#     import time
+#
+#     coordinates = random.choice(free_cells)
+#     print('Making move level "easy"')
+#     time.sleep(0.9)
+#     move(coordinates, symbol)
+
+def check_move(coordinates, symbol):
     global count_moves
+    count_moves += 1
 
-    try:
-        x, y = map(int, coordinates.split())
-    except Exception:
-        print('You should enter numbers!')
-    else:
-        if not (x in range(1, size + 1) and y in range(1, size + 1)):
-            print(f'Coordinates should be from 1 to {size}!')
-        elif state_table[x - 1][y - 1] == ' ':
-            state_table[x - 1][y - 1] = symbol
-            illustrate(state_table)
+    x, y = coordinates
+    state_table[x - 1][y - 1] = symbol
+    free_cells.remove([x, y])
 
-            count_moves += 1
-            free_cells.remove(f'{x} {y}')
+    illustrate(state_table)
 
-            return True
+    won = win(state_table, symbol)
+    if won or count_moves == size ** 2:
+        if won:
+            print(f'{symbol} wins')
         else:
-            print('This cell is occupied! Choose another one!')
+            print('Draw')
 
+        return False
 
-def easy_move(symbol):
-    import random
-    import time
-
-    coordinates = random.choice(free_cells)
-    print('Making move level "easy"')
-    time.sleep(0.9)
-    move(coordinates, symbol)
+    return True
 
 
 def win(state_table, symbol):
@@ -81,33 +111,51 @@ def win(state_table, symbol):
     return False
 
 
+def first_command():
+    import sys
+
+    while True:
+        command = input('Input command: ').lower()
+
+        if command == 'exit':
+            sys.exit()
+
+        command = command.split()
+        if len(command) == 3 and command[0] == 'start' and \
+                command[1] in available_players and command[2] in available_players:
+            return [create_player(command[i + 1]) for i in range(2)]
+        else:
+            print('Bad parameters!')
+
+
+def create_player(name):
+    if name == 'user':
+        return User()
+    if name == 'easy':
+        return Easy()
+
+
 size = 3
+available_players = {'user', 'easy'}
+symbols = ['X', 'O']
 
 count_moves = 0
 
 initial_state = '_' * size ** 2
-free_cells = [f'{i + 1} {j + 1}' for i in range(size) for j in range(size)]
+free_cells = [[i + 1, j + 1] for i in range(size) for j in range(size)]
 
 state_table = create_table(initial_state)
-illustrate(state_table)
 
 game = True
 
-while game:
-    symbol = 'X' if count_moves % 2 == 0 else 'O'
+if __name__ == '__main__':
+    while True:
+        players = first_command()  # start user easy
+        print(type(players[0]))
+        print(type(players[1]))
 
-    if symbol == 'X':
-        coordinates = input('Enter the coordinates: ')
-        while not move(coordinates, symbol):
-            coordinates = input('Enter the coordinates: ')
-    else:
-        easy_move(symbol)
-
-    won = win(state_table, symbol)
-    if won or count_moves == size ** 2:
-        if won:
-            print(f'{symbol} wins')
-        else:
-            print('Draw')
-        game = False
-        break
+        while game:
+            for i in range(2):
+                coordinates = players[i].move()
+                symbol = symbols[i]
+                game = check_move(coordinates, symbol)
